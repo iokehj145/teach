4c62a8c2c2ea648919c4721f195462e8fcff9682
iokehj145 <61664214+iokehj145@users.noreply.github.com>
2023-06-15 22:11:31 +0300

    OOP

--- test.py
+++ test.py
@@ -1,79 +1,88 @@
 import random
 import time
 class Sort:
-    def mk_arr(n):# Створення массиву
-        array = list(range(1, n + 1))
-        random.shuffle(array)
-        return array
-    
-    def fun_sel(fun, array, n): # функція для виклику інших функцій
+    def __init__(self, n): # ініціалізація масиву
+        self.n = n
+        self.array = list(range(1, self.n + 1))
+        random.shuffle(self.array)
+
+    def fun_sel(self, fun): # функція для виклику інших функцій
         start = time.time()
         if fun == 1:
-         array = Sort.quicksort(array, n)
+         self.quicksort()
         elif fun == 2:
-         array = Sort.sel_sort(array, n)
+         self.sel_sort()
         elif fun == 3:
-         array = Sort.insertion_sort(array, n)
+         self.insertion_sort()
         elif fun == 4:
-         array = Sort.bogoSort(array, n)
+         self.bogoSort()
         elif fun == 5:
-         array = Sort.bubble_sort(array, n)
+         self.bubble_sort()
         count = round(time.time() - start, 4)
-        return array, count
+        return self.array, count
 
-    def insertion_sort(array, n): # сортування вставками
-        for i in range(1, n):
-            key = array[i]
-            j = i - 1
-            while j >= 0 and array[j] > key:
-                array[j + 1] = array[j]
-                j -= 1
-            array[j + 1] = key
-        return array
+    def insertion_sort(self):  # сортування вставками
+        for i in range(1, self.n):
+          key = self.array[i]
+          j = i - 1
+          while j >= 0 and self.array[j] > key:
+            self.array[j + 1] = self.array[j]
+            j -= 1
+          self.array[j + 1] = key
 
-    def sel_sort(array, n): # Вибіркове сортування
-        for i in range(n):
+    def sel_sort(self):  # Вибіркове сортування
+        for i in range(self.n):
             minimal = i
-            for j in range(i + 1, n):
-                if array[j] < array[minimal]:
+            for j in range(i + 1, self.n):
+                if self.array[j] < self.array[minimal]:
                     minimal = j
-            array[minimal], array[i] = array[i], array[minimal]
-        return array
+            self.array[minimal], self.array[i] = self.array[i], self.array[minimal]
 
-    def quicksort(array, n): # Швидке сортування
-     if n <= 1:
-         return array
-     pivot = array[n // 2]
-     left = [x for x in array if x < pivot]
-     middle = [x for x in array if x == pivot]
-     right = [x for x in array if x > pivot]
-     return Sort.quicksort(left, len(left)) + middle + Sort.quicksort(right, len(right))
 
-    def cycle(n, arr, check):  # бульбашкове сортування
-      check = True
-      for i in range(0, n-1):
-         if arr[i] > arr[i+1]:
-           arr[i],arr[i+1] = arr[i+1],arr[i]
-           check = False
-      return arr, check
-    def bubble_sort(arr, n):
-     check = False
-     while check == False:
-       arr, check = Sort.cycle(n, arr, check)
-     return arr
+    def quicksort(self):  # Швидке сортування
+        self.quicksort1(0, self.n - 1)
+
+    def quicksort1(self, low, high):
+        if low < high:
+            pivot_index = self.partition(low, high)
+            self.quicksort1(low, pivot_index - 1)
+            self.quicksort1(pivot_index + 1, high)
+
+    def partition(self, low, high):
+        pivot = self.array[high]
+        i = low - 1
+        for j in range(low, high):
+            if self.array[j] <= pivot:
+                i += 1
+                self.array[i], self.array[j] = self.array[j], self.array[i]
+        self.array[i + 1], self.array[high] = self.array[high], self.array[i + 1]
+        return i + 1
+
+    def cycle(self):  # бульбашкове сортування
+        check = True
+        for i in range(0, self.n - 1):
+            if self.array[i] > self.array[i + 1]:
+                self.array[i], self.array[i + 1] = self.array[i + 1], self.array[i]
+                check = False
+        return check
+    def bubble_sort(self):
+        check = False
+        while check is False:
+            check = self.cycle()
     
-    def bogoSort(array, n): # Бого сорт
-     while Sort.bog_check(array, n)==False:
-        random.shuffle(array)
-     return array
-    def bog_check(array, n):
-     for i in range(0, n-1):
-         if (array[i] > array[i+1] ):
-             return False
-     return True
-n = int(input("Число елементів масиву: "))
-array = Sort.mk_arr(n)
-fun = int(input("1--quicksort, 2--selection sort, 3--insertion sort, \n4--Bogo sort, 5--bubble sort\nОберість функцію: "))
-print('Не відсортований масив: ', array)
-array, count =Sort.fun_sel(fun, array, n)
-print('А теперь відсортований: ', array, '\nЧас:', count)
+    def bogoSort(self):  # Бого сорт
+        while self.bog_check() is False:
+            random.shuffle(self.array)
+    def bog_check(self):
+        for i in range(0, self.n - 1):
+            if self.array[i] > self.array[i + 1]:
+                return False
+        return True
+
+while True:
+  n = int(input("Число елементів масиву: "))
+  array = Sort(n)
+  fun = int(input("1--quicksort, 2--selection sort, 3--insertion sort, \n4--Bogo sort, 5--bubble sort\nОберість функцію: "))
+  print('Не відсортований масив: ', array.array)
+  array, count =array.fun_sel(fun)
+  print('А теперь відсортований: ', array, '\nЧас:', count)
